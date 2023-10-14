ad16a820f09903261b72e744307e42c6c3f564c2
kris0395 <khrispitbrown@gmail.com>
2023-10-13 21:39:57 -0500

    actualizacion main 1.6

--- upcomingEvents.js
+++ upcomingEvents.js
@@ -18,7 +18,7 @@ for (let evento of eventos) {
 
         card.innerHTML = `<img src="${evento.image}" class="card-img-top h-50" alt="...">
                     <div class="card-body">
-                    <h5 class="card-title">${evento.name}</h5>
+                    <h5 class="card-title fonts">${evento.name}</h5>
                     <p class="card-text text-truncate">${evento.description}</p>
                     <div class="d-flex justify-content-around">
                         <p class="card-text"> $ ${evento.price}</p>
